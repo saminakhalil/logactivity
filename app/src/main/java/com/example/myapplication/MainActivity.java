7eaf001d6d478ab4aa2c77da8985982d96653660
saminakhalil <skdigi2020@gmail.com>
2021-03-03 00:37:31 +0500

    test

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,18 +5,52 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.textfield.TextInputLayout;
+
+public class MainActivity<getDetail> extends AppCompatActivity {
+    EditText emailet;
+    EditText psaaet;
+    TextInputLayout emailtv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initilization();
 
     }
 
+    private void initilization() {
+                emailet =findViewById(R.id.emailbt);
+                psaaet=findViewById(R.id.passbts);
+                emailtv=findViewById(R.id.emaillo);
+    }
+         private void getDetailt(){
+        String email = emailet.getText().toString();
+             String pass = psaaet.getText().toString();
+             if(email.isEmpty()){
+                 emailet.setError("email required");
+             }
+             else if(pass.isEmpty())
+             {psaaet.setError("password required");
+             }
+             else{
+                 Intent intend1 = new Intent(MainActivity.this,profile.class);
+                 startActivity(intend1);
+             }
+
+         }
+
     public void login(View view) {
-        Intent intend1 = new Intent(MainActivity.this,profile.class);
-        startActivity(intend1);
+        getDetailt();
+
+
+    }
+
+    public void signup(View view) {
+        Intent sign = new Intent(MainActivity.this,signup.class);
+        startActivity(sign);
     }
 }
